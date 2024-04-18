b1aed3cb019e22706b23e9b03505c8ca7112eebb
eirik <bjoroen.eirik@gmail.com>
2024-04-19 00:04:55 +0200

    More test refactor
    Refactored tests to be clearer and easier to write

--- src/assembler/lexer.rs
+++ src/assembler/lexer.rs
@@ -37,7 +37,7 @@ impl<'a> Lexer<'a> {
     pub fn lex(&mut self) {
         while let Some(c) = self.next() {
             match c {
-                '#' => self.parse_operand(),
+                '#' => self.parse_int_operand(),
                 '$' => self.parse_register(),
                 _ if c.is_alphabetic() => self.parse_opcode(c),
                 _ if c.is_whitespace() => {}
@@ -48,43 +48,59 @@ impl<'a> Lexer<'a> {
 
     fn parse_opcode(&mut self, c: char) {
         let mut s = String::from(c);
-        let line = self.current_line;
 
         while let Some(c) = self.next() {
             if c.is_alphabetic() {
                 s.push(c)
             } else {
+                assert!(c.is_whitespace());
                 break;
             }
         }
 
         if let Ok(opcode) = Opcode::from_str(&s.to_lowercase()) {
-            let token = Token::Op { code: opcode, line };
+            let token = Token::Op {
+                code: opcode,
+                line: self.current_line,
+            };
             self.tokens.push(token)
         } else {
-            eprintln!("Unknown Opcode: {} on line {}", s, self.current_line)
+            panic!("Unknown Opcode: {} on line {}", s, self.current_line)
         }
     }
 
     fn parse_register(&mut self) {
         let mut s = vec![];
-        let line = self.current_line;
 
         while let Some(c) = self.next() {
             if c.is_alphanumeric() {
                 s.push(c)
             } else {
+                assert!(c.is_whitespace());
                 break;
             }
         }
 
         let register = String::from_iter(s);
 
-        self.tokens.push(Token::Register { register, line })
+        self.tokens.push(Token::Register { register })
     }
 
-    fn parse_operand(&self) {
-        todo!()
+    fn parse_int_operand(&mut self) {
+        let mut s = String::new();
+
+        while let Some(c) = self.next() {
+            dbg!(&c);
+            if c.is_numeric() {
+                s.push(c)
+            } else {
+                break;
+            }
+        }
+
+        self.tokens.push(Token::IntOperand {
+            operand: s.parse().unwrap(),
+        })
     }
 }
 
@@ -93,27 +109,52 @@ mod tests {
 
     use super::*;
 
-    fn run_test(test_cases: &[(&str, Vec<&str>)]) {
-        for case in test_cases {
-            let mut lexer = Lexer::new(case.0);
+    fn run_test(test_cases: &[(&str, &str)]) {
+        for (input, expected) in test_cases {
+            let mut lexer = Lexer::new(input);
             lexer.lex();
 
-            for (i, token) in lexer.tokens.into_iter().enumerate() {
-                assert_eq!(token.to_string(), case.1[i])
-            }
+            let token_string = String::from_iter(lexer.tokens.iter().map(|t| t.to_string()));
+
+            assert_eq!(token_string, *expected)
         }
     }
 
     #[test]
     fn test_lex_opcode() {
-        let test_cases = [("LOAD\nADD]\nSUB", vec!["1:load", "2:add", "3:sub"])];
+        let test_cases = [
+            ("LOAD", "1:load "),
+            ("\nADD", "2:add "),
+            ("\n\nSUB", "3:sub "),
+        ];
 
         run_test(&test_cases)
     }
 
     #[test]
     fn test_lex_register() {
-        let test_cases = [("$10\n$hello\n$world", vec!["10", "hello", "world"])];
+        let test_cases = [("$10", "10 "), ("$hello", "hello "), ("$world", "world ")];
+
+        run_test(&test_cases)
+    }
+
+    #[test]
+    fn test_lex_operand() {
+        let test_cases = [("#10", "10 "), ("#20", "20 "), ("#30", "30 ")];
+
+        run_test(&test_cases)
+    }
+
+    #[test]
+    fn test_lex_full_line() {
+        let test_cases = [
+            ("load $hello #10", "1:load hello 10 "),
+            ("add $result $hello #1", "1:add result hello 1 "),
+            (
+                "sub $result #1 #2\n add $result #30 #20",
+                "1:sub result 1 2 2:add result 30 20 ",
+            ),
+        ];
 
         run_test(&test_cases)
     }
--- src/assembler/mod.rs
+++ src/assembler/mod.rs
@@ -7,16 +7,16 @@ mod lexer;
 #[derive(Debug, PartialEq)]
 pub enum Token {
     Op { code: Opcode, line: usize },
-    Register { register: String, line: usize },
-    IntOperand { operand: usize, line: usize },
+    Register { register: String },
+    IntOperand { operand: usize },
 }
 
 impl Display for Token {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Token::Op { code, line } => write!(f, "{}:{}", line, code),
-            Token::Register { register, line: _ } => write!(f, "{}", register),
-            Token::IntOperand { operand, line: _ } => write!(f, "{}", operand),
+            Token::Op { code, line } => write!(f, "{}:{} ", line, code),
+            Token::Register { register } => write!(f, "{} ", register),
+            Token::IntOperand { operand } => write!(f, "{} ", operand),
         }
     }
 }
