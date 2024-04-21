956b2b8f5b50e137262e67abccec6f0d90ac6af9
eirik <bjoroen.eirik@gmail.com>
2024-04-21 11:39:27 +0200

    Parser now groups tokens into one instruction

--- src/assembler/parser.rs
+++ src/assembler/parser.rs
@@ -33,7 +33,7 @@ impl Parser {
     pub fn parse(&mut self) {
         self.read();
 
-        if let Some(instruction) = self.next() {
+        while let Some(instruction) = self.next() {
             self.program.push(instruction);
         }
     }
@@ -41,11 +41,13 @@ impl Parser {
     fn parse_instruction(&mut self) -> Option<AssemblerInstruction> {
         match self.current {
             Token::Op { code: _ } => {
+                let op = self.current;
+
                 // Eat the OP token
                 self.read();
 
                 Some(AssemblerInstruction {
-                    opcode: self.current,
+                    opcode: op,
                     operand_one: self.next_operand(),
                     operand_two: self.next_operand(),
                     operand_three: self.next_operand(),
@@ -83,6 +85,7 @@ impl Parser {
     /// Reads and eats the next token
     pub fn read(&mut self) {
         self.current = if let Some(token) = self.lexer.next() {
+            dbg!(&token);
             token
         } else {
             Token::EOF
@@ -97,10 +100,46 @@ mod tests {
 
     #[test]
     fn test_parse_instruction() {
-        let mut p = Parser::new("load $10 #10");
+        let mut p = Parser::new("load $10 #10\nHLT\nADD $0 #10 #10");
         p.parse();
         let instructions = p.get_instructions();
 
-        assert_eq!(instructions.len(), 1)
+        assert_eq!(instructions.len(), 3);
+
+        assert_eq!(
+            instructions[0],
+            AssemblerInstruction {
+                opcode: Token::Op {
+                    code: crate::instruction::Opcode::LOAD
+                },
+                operand_one: Some(Token::Register { register: 10 }),
+                operand_two: Some(Token::IntOperand { operand: 10 }),
+                operand_three: None
+            }
+        );
+
+        assert_eq!(
+            instructions[1],
+            AssemblerInstruction {
+                opcode: Token::Op {
+                    code: crate::instruction::Opcode::HLT
+                },
+                operand_one: None,
+                operand_two: None,
+                operand_three: None
+            }
+        );
+
+        assert_eq!(
+            instructions[2],
+            AssemblerInstruction {
+                opcode: Token::Op {
+                    code: crate::instruction::Opcode::ADD
+                },
+                operand_one: Some(Token::Register { register: 0 }),
+                operand_two: Some(Token::IntOperand { operand: 10 }),
+                operand_three: Some(Token::IntOperand { operand: 10 }),
+            }
+        )
     }
 }
