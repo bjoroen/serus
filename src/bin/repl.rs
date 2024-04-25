eb8042185d542399455f3762cad7a9cd861f0f75
eirik <bjoroen.eirik@gmail.com>
2024-04-25 21:43:12 +0200

    Added way to print state of registers and program, for easier debugging

--- src/bin/repl.rs
+++ src/bin/repl.rs
@@ -1,4 +1,4 @@
-use std::{io::stdin, process::exit};
+use std::io::{stdin, stdout, Write};
 
 use lib::{assembler::program::Program, vm::VM};
 
@@ -19,28 +19,43 @@ impl REPL {
                 eprintln!("Could not read from stdin: {e}");
                 break;
             }
-
-            if buffer.starts_with("!") {
+            if buffer.starts_with(":") {
                 self.run_command(&buffer)
             } else {
                 let mut program = Program::parse_program(&buffer);
                 self.vm.program.append(&mut program);
                 self.vm.run_once();
-                dbg!("{}", self.vm.registers);
             }
         }
     }
 
     fn run_command(&self, input: &str) {
-        match input {
-            "!Q" => self.quit(),
+        match input.trim_end() {
+            ":quit" | ":q" => self.quit(),
+            ":registers" | ":r" => self.show_registers(),
+            ":program" | ":p" => self.show_program(),
             _ => println!("{input}"),
         }
     }
 
     fn quit(&self) {
         println!("Good bye, happy coding! :D");
-        exit(0)
+        std::process::exit(0)
+    }
+
+    fn show_registers(&self) {
+        for (index, register) in self.vm.registers.iter().enumerate() {
+            println!("Register: {} -> {}", index, register)
+        }
+    }
+
+    fn show_program(&self) {
+        let chunks: Vec<&[u8]> = self.vm.program.chunks(4).collect();
+        for chunk in chunks {
+            chunk.iter().for_each(|c| print!("{}", c));
+            println!();
+            println!("-------------");
+        }
     }
 }
 
